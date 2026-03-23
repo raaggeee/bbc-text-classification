10104945bafe35817e77bcbe879e6b677b7e2b36
raaggeee <raaggee08@gmail.com>
2026-03-23 23:29:11 +0530

    added ingestion pipeline

--- src/data_ingestion/ingest.py
+++ src/data_ingestion/ingest.py
@@ -1,3 +1,28 @@
 import numpy as np
-import pandas as pd
 from sklearn.model_selection import train_test_split
+from utils import open_df, open_yaml, save_df
+
+def loads_cofigs(yaml):
+    save_path = yaml["data_ingestion"]["path"]
+
+    return save_path
+
+def split_data(df):
+    train_data, test_data = train_test_split(df, test_size=0.2, random_state=42)
+
+    return train_data, test_data
+
+def main():
+    yaml_file = open_yaml("config.yaml")
+    save_path = loads_cofigs(yaml_file)
+
+    df = open_df("data/bbc_data.csv")
+
+    train, test = split_data(df)
+
+    save_df(save_path, "train_df", train)
+    save_df(save_path, "test_df", test)
+
+if __name__ == "__main__":
+    main()
+
