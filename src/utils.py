a3c3b788ec895abab198ddb0572000726efd3cf3
raaggeee <raaggee08@gmail.com>
2026-03-23 23:32:44 +0530

    added util functions

--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,19 @@
 import yaml
 import os
+import pandas as pd
 
 def open_yaml(file_path):
-    pass
+    with open(file_path, "r") as f:
+        yaml_file = yaml.safe_load(f)
+
+    return yaml_file
 
 def open_df(file_path):
-    pass
+    df = pd.read_csv(file_path)
+    return df
 
-def save_df(file_path):
-    pass
+def save_df(file_path, filename, df):
+    os.makedirs(file_path, exist_ok=True)
+    save_path = os.path.join(file_path, filename)
+    df.to_csv(save_path, index=False)
+    return True
